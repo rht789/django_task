892bfbbdc5906157ee5ccee1d2059cb3614e6501
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2025-01-28 18:13:55 +0600

    Initial Project Done

---
+++ tasks/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from tasks.views import show_task
+
+urlpatterns = [
+    path('show_task/', show_task)
+]
---
+++ tasks/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    #Work with db
+    #transform data
+    #Data pass
+    #http response// json response
+    return HttpResponse("Welcome to the task management system")
+def contact(request):
+     return HttpResponse("<h1 style = 'color : red'>This is Contact</h1> <br> Hi")
+# must use single quotation inside double quotation
+
+def show_task(request):
+    return HttpResponse("This is our task")
