a7f1c94b32e350dd3b2099cbc7bbb16c91892d38
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2025-01-30 01:18:10 +0600

    module 4.2 - Template inheritance and related tags

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from tasks.views import show_task,show_specific_task
+from tasks.views import manager_dashboard,user_dashboard
 
 urlpatterns = [
-    path('show_task/', show_task),
-    path('show_task/<int:id>', show_specific_task) #jei datatype nibo sheita lekhte hbe routes ey
+    # path('show_task/<int:id>', show_specific_task) #jei datatype nibo sheita lekhte hbe routes ey
+    path('dashboard/', manager_dashboard),
+    path('user_dashboard/', user_dashboard)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -8,11 +8,7 @@ def home(request):
     #Data pass
     #http response// json response
     return HttpResponse("Welcome to the task management system")
-def contact(request):
-     return HttpResponse("<h1 style = 'color : red'>This is Contact</h1> <br> Hi")
-# must use single quotation inside double quotation
-
-def show_task(request):
-    return HttpResponse("This is our task")
-def show_specific_task(request,id):
-    return HttpResponse(f'This is our task no. {id}')
+def manager_dashboard(request):
+    return render(request, "dashboard/manager_dashboard.html")
+def user_dashboard(request):
+    return render(request, "dashboard/user_dashboard.html")
