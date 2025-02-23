72fd2e5557502dd9533be21339a45e279573c99e
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2025-02-23 18:41:55 +0600

    10.1 done

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tasks.models import *
 
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(TaskDetail)
+admin.site.register(Employee)
+admin.site.register(Project)
