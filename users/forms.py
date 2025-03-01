1ad136f412ae8bb9b9d31feefb347f2fa262b681
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2025-03-01 21:50:02 +0600

    10.4

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -10,3 +11,24 @@ class RegisterForm(UserCreationForm):
         super(UserCreationForm, self).__init__(*args, **kwargs)
         for fieldname in ['username', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
+            
+class CustomRegisterForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['username','first_name','last_name','password1','password2','email']
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if not password1:
+            raise forms.ValidationError("Password is required.")
+        if len(password1) < 8:
+            raise forms.ValidationError("Password must be at least 8 characters long.")
+        if not any(char.isupper() for char in password1):
+            raise forms.ValidationError("Password must contain at least one uppercase letter.")
+        if not any(char.isdigit() for char in password1):
+            raise forms.ValidationError("Password must contain at least one number.")
+        if not any(char in '!@#$%^&*()' for char in password1):
+            raise forms.ValidationError("Password must contain at least one special character (e.g., !@#$%^&*()).")
+        return password1
--- users/views.py
+++ users/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-from users.forms import RegisterForm
+from users.forms import RegisterForm,CustomRegisterForm
 
 
 # Create your views here.
 def signup(request):
     if request.method == 'GET':
-        form = RegisterForm()
+        form = CustomRegisterForm()
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
+        form = CustomRegisterForm(request.POST)
         if form.is_valid():
             form.save()
         else:
