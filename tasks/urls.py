d71f7e50851d9516b380bbf1d940d971c5a85839
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2025-03-19 00:07:50 +0600

    bug fix

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from tasks.views import manager_dashboard,employee_dashboard,ViewProject,TaskDetails,UpdateClass,delete_task,dashboard,CreateTask
+from tasks.views import manager_dashboard,employee_dashboard,ViewProject,TaskDetails,UpdateTask,delete_task,dashboard,CreateTask
 
 urlpatterns = [
     # path('show_task/<int:id>', show_specific_task) #jei datatype nibo sheita lekhte hbe routes ey
@@ -8,7 +8,7 @@ urlpatterns = [
     path('create_task/', CreateTask.as_view(), name='create_task'),
     path('view_projects/', ViewProject.as_view(), name='view_projects'),
     path('task/<int:task_id>/details', TaskDetails.as_view(), name='task_details'),
-    path('update_task/<int:id>', UpdateClass.as_view(), name='update_task'),
+    path('update_task/<int:id>', UpdateTask.as_view(), name='update_task'),
     path('delete_task/<int:id>', delete_task, name='delete_task'),
     path('dashboard', dashboard, name='dashboard')
 ]
