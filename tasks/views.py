1def9b554ca586e53adfbeae7b481a0a071c9808
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2025-03-19 00:00:02 +0600

    d15.7 updateView

--- tasks/views.py
+++ tasks/views.py
@@ -9,7 +9,7 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.base import ContextMixin
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
 
 # Create your views here.
 def is_admin(user):
@@ -110,36 +110,43 @@ class CreateTask(ContextMixin,LoginRequiredMixin,PermissionRequiredMixin,View):
             return render(request,self.template_name, context)  
 
 
-update_decorators = [login_required,permission_required("tasks.change_task", login_url='no-permission')]
-
-@method_decorator(update_decorators,name='dispatch')
-
-class UpdateClass(View):
+class UpdateTask(UpdateView):
+    model = Task
+    form_class = TaskModelForm
     template_name = 'task_form.html'
-    def get(self, request, *args, **kwargs):
-        task = Task.objects.get(id=id)
-        task_form = TaskModelForm(instance=task)
-        
-        if task.details:
-            taskdetail_form = TaskDetailModelForm(instance=task.details)
-        context = {
-        "task_form" : task_form,
-        "taskdetail_form" : taskdetail_form
-        }
-        return render(request,self.template_name, context)
-    def post(self, request, *args, **kwargs):
-        task_form = TaskModelForm(request.POST, instance=task)
-        taskdetail_form = TaskDetailModelForm(request.POST, instance=task.details)
-        if task_form.is_valid() and taskdetail_form.is_valid():
-            """For Django model Form"""
-            task = task_form.save()
-            taskdetail = taskdetail_form.save(commit=False)
-            taskdetail.task = task
-            taskdetail.save()
-            
-            messages.success(request, "Task Updated Successfully")
-            return redirect('update_task',id)
+    context_object_name = 'task'
+    pk_url_kwarg = 'id'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task_form'] = self.get_form()
+        print(context)
+        if hasattr(self.object, 'details') and self.object.details:
+            context['task_detail_form'] = TaskDetailModelForm(
+                instance=self.object.details)
+        else:
+            context['task_detail_form'] = TaskDetailModelForm()
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        task_form = TaskModelForm(request.POST, instance=self.object)
+
+        task_detail_form = TaskDetailModelForm(
+            request.POST, request.FILES, instance=getattr(self.object, 'details', None))
+
+        if task_form.is_valid() and task_detail_form.is_valid():
+
+            """ For Model Form Data """
+            task = task_form.save()
+            task_detail = task_detail_form.save(commit=False)
+            task_detail.task = task
+            task_detail.save()
+
+            messages.success(request, "Task Updated Successfully")
+            return redirect('update-task', self.object.id)
+        return redirect('update-task', self.object.id)
 @login_required
 @permission_required("tasks.delete_task", login_url='no-permission')
 def delete_task(request,id):
